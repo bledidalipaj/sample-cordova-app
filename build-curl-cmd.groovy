2e115968ce252e71798ef15dc417d6e78b363c90
bdalipaj <bdalipaj@zafire.com>
2022-06-02 11:23:25 +0300

    Fix curl command

--- build-curl-cmd.groovy
+++ build-curl-cmd.groovy
@@ -1,7 +1,7 @@
 String buildCurlCmd(String url, String fileName = '') {
   String cmd = "curl $url"
 
-  cmd = (fileName != '') ? cmd + "-o $fileName" : cmd
+  cmd = (fileName != '') ? cmd + " -o $fileName" : cmd
 
   return cmd
 }
